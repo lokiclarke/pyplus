8fddd3d8e7e7b153885f0fe9ce878447fe44ab04
lokiclarke <100521828+lokiclarke@users.noreply.github.com>
2022-10-04 10:02:03 -0400

    Update version & keywords

--- pyplus/setup.py
+++ pyplus/setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyPlus',
-    version='1.0.0',
+    version='1.0.3',
     license='MIT',
     author="SSoloSkrips",
     author_email='lokiclarke09@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/lokiclarke/pyplus',
-    keywords=['batch','plus','extra'],
+    keywords=['shell','batch','plus','extra','premium'],
     install_requires=['pyfiglet',]
 )
